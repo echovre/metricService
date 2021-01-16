5d408b7066a74918f15cd109c4278399ca3c7d6c
echovre <you@example.com>
2021-01-16 09:18:43 -0700

    use filter endpoint

--- app.py
+++ app.py
@@ -3,41 +3,33 @@ import os, sys, json
 import urllib3
 from flask import Flask, request, jsonify
 app = Flask(__name__)
+http = urllib3.PoolManager()
 
-#for now, load everything into memory. Not sure if Im supposed to re-query the list every time or store in some database if the list is big
-#(also, heroku apps have 30 sec timeout unless I want to go through the trouble of setting up background jobs)
+#https://housekeeping.vacasa.io/cleans?filter[id][in]=16a92bf5-0e5d-4372-a801-1d4e2895be65,e3e70682-c209-4cac-629f-6fbed82c07cd
 
-#not sure how to interpret the parameters list at:
 #https://housekeeping.vacasa.io/#tag/cleans
-#also seems to return forbidden?:
-#https://housekeeping.vacasa.io/#tag/clean_times
 
 CLEANS_URL='https://housekeeping.vacasa.io/cleans'
 
-http = urllib3.PoolManager()
-response = http.request('GET', CLEANS_URL)
-responseData=json.loads(response.data)
-
-timesDict={}
-for each in responseData['data']:
-    id=each['id']
-    time=each['attributes']['predicted_clean_time']
-    timesDict[id]=time
-
-def getMetrics(idsList):
+def getMetrics(cleansData):
+    timesDict={}
+    for each in cleansData:
+        myId=each['id']
+        cleanTime=each['attributes']['predicted_clean_time']
+        timesDict[myId]=cleanTime
+    
     firstTime=timesDict[list(timesDict.keys())[0]]
     minTime=firstTime
     maxTime=firstTime
     totalTime=0
     count=0
 
-    for each in idsList:
-        if each in timesDict:
-            cleanTime=timesDict[each]
-            totalTime+=cleanTime
-            if minTime>cleanTime: minTime=cleanTime 
-            if maxTime<cleanTime: maxTime=cleanTime 
-            count+=1
+    for each in timesDict:
+        cleanTime=timesDict[each]
+        totalTime+=cleanTime
+        if minTime>cleanTime: minTime=cleanTime 
+        if maxTime<cleanTime: maxTime=cleanTime 
+        count+=1
 
     metrics={}
     metrics["maxCleanTime"]=maxTime
@@ -53,14 +45,24 @@ def doMetric(uuid):
     #"16a92bf5-0e5d-4372-a801-1d4e2895be65",
     #"4ffaecc0-3047-4da7-abe2-dd4163f17e61"];
     #metrics=getMetrics(inputs)
-    
-    #TODO: error check uuid
     metrics=getMetrics([uuid])
     return jsonify(metrics)
     
 #curl -X POST http://127.0.0.1:5000/ -H 'Content-Type: application/json' -d '{"ids":["e3e70682-c209-4cac-629f-6fbed82c07cd", "16a92bf5-0e5d-4372-a801-1d4e2895be65", "4ffaecc0-3047-4da7-abe2-dd4163f17e61"]}'
 @app.route('/', methods=['POST'])
 def doMetrics():
-    #TODO: error check request
     content = request.json
-    return getMetrics(content['ids'])
+    urlParams="?filter[id][in]="
+    #todo error check content[ ids]
+    for myId in content['ids']:
+        urlParams+=myId
+        urlParams+=","
+    urlParams=urlParams.rstrip(",") #strip last comma
+    myUrl=CLEANS_URL+urlParams
+    cleansResponse = http.request('GET', myUrl)
+    responseData=json.loads(cleansResponse.data)
+    #TODO: error check request
+    return getMetrics(responseData['data'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
